6942c1657d7d73de5ac6bde268e8e6227e26e8d9
Jorge Corro <jorge@xpresa.mx>
2026-02-27 16:55:22 -0600

    Allow api1.xpresa.com.mx in head next.config images

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/storage/v1/object/public/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'api1.xpresa.com.mx',
+                port: '',
+                pathname: '/storage/v1/object/public/**',
+            },
         ],
     },
 };
